f338d61e2e0623929ea117c7ee0f73a0e743c074
s30402 <146550889+s30402@users.noreply.github.com>
2023-12-14 19:44:07 +0100

    Zad 2

---
+++ PPRG_5_CW/Zad2/Zad2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void mnozenie();
+
+int wynik[2][2];
+int MCZ_1[2][3]{ {6, 12, 3} , {2, -5, 1} };
+int MCZ_2[3][2]{ {-15, 9}, {1,-4}, {4, 7} };
+
+
+int main()
+{
+	mnozenie();
+	cout << wynik[0][0] << " " << wynik[0][1] << endl;
+	cout << wynik[1][0] << " " << wynik[1][0];
+
+	return 0;
+
+}
+
+void mnozenie() {
+	wynik[0][0] = MCZ_1[0][0] * MCZ_2[0][0] + MCZ_1[0][1] * MCZ_2[1][0] + MCZ_1[0][2] * MCZ_2[2][0];
+	wynik[0][1] = MCZ_1[0][0] * MCZ_2[0][1] + MCZ_1[0][1] * MCZ_2[1][1] + MCZ_1[0][2] * MCZ_2[2][1];
+	wynik[1][0] = MCZ_1[1][0] * MCZ_2[0][0] + MCZ_1[1][1] * MCZ_2[1][0] + MCZ_1[1][2] * MCZ_2[2][0];
+	wynik[1][1] = MCZ_1[1][0] * MCZ_2[0][1] + MCZ_1[1][1] * MCZ_2[1][1] + MCZ_1[1][2] * MCZ_2[2][1];
+}
